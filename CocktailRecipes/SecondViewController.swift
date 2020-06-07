389f1151d7125bbc12b90a2ec2b6334d1472d373
Jan Gundorf <jgundorf@gnresound.com>
2020-06-07 09:26:33 +0200

    Initial Commit

---
+++ CocktailRecipes/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CocktailRecipes
+//
+//  Created by Jan Gundorf on 07/06/2020.
+//  Copyright © 2020 Jan Gundorf. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
