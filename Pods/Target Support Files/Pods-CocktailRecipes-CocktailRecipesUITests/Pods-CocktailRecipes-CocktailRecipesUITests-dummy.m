1552c419d001571137b9e844f1cdc8e0662ffab2
Jan Gundorf <jgundorf@gnresound.com>
2020-06-28 21:26:33 +0200

    Push graphic

---
+++ Pods/Target Support Files/Pods-CocktailRecipes-CocktailRecipesUITests/Pods-CocktailRecipes-CocktailRecipesUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CocktailRecipes_CocktailRecipesUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CocktailRecipes_CocktailRecipesUITests
+@end
